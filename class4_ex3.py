db2f3927fa6b8ff5c501cc51ff83e848578b9319
hgbenga <ohassan@hawk.iit.edu>
2015-05-21 10:55:01 -0400

    Update class4_ex3.py

--- class4_ex3.py
+++ class4_ex3.py
@@ -1,3 +1,6 @@
+arista_vlan.yml
+==========================================
+
 ---
 
 - name: Create Arista VLANs
@@ -41,3 +44,74 @@
             eapi_password={{ eapi_password }}
             eapi_hostname={{ eapi_hostname }}
             eapi_port={{ eapi_port }}
+            
+            
+            
+  arista_coomand.yml
+  ===================================
+  
+  ---
+
+- name: Arista Ansible testing
+  hosts: arista
+
+  tasks:
+    - name: Testing command (not idempotent)
+      eos_command: commands="write memory"
+        eapi_username={{ eapi_username }}
+                eapi_password={{ eapi_password }}
+                eapi_hostname={{ eapi_hostname }}
+                eapi_port={{ eapi_port }}
+      register: cmd_out
+    - debug: var=cmd_out
+
+
+arista_cleanup.yml
+=======================================
+
+---
+
+- name: Arista Ansible testing
+  hosts: arista
+
+  tasks:
+    - name: Testing command (not idempotent)
+      eos_command: commands="write memory"
+        eapi_username={{ eapi_username }}
+                eapi_password={{ eapi_password }}
+                eapi_hostname={{ eapi_hostname }}
+                eapi_port={{ eapi_port }}
+      register: cmd_out
+    - debug: var=cmd_out
+(applied_python)[ohassan@ip-172-30-0-134 ARISTA]$ cat arista_cleanup.yml
+---
+
+- name: Arista Cleanup
+  hosts: arista
+
+  tasks:
+    - name: Configure Ethernet7 for access mode, VLAN1
+      eos_switchport: name=Ethernet7 mode=access access_vlan=1 trunk_allowed_vlans=all trunk_native_vlan=1
+            eapi_username={{ eapi_username }}
+            eapi_password={{ eapi_password }}
+            eapi_hostname={{ eapi_hostname }}
+            eapi_port={{ eapi_port }}
+
+    - name: Setting Ethernet7 description
+      eos_interface: name=Ethernet7 admin=enable null_as_default=true
+            eapi_username={{ eapi_username }}
+            eapi_password={{ eapi_password }}
+            eapi_hostname={{ eapi_hostname }}
+            eapi_port={{ eapi_port }}
+
+    - name: Remove VLANs
+      eos_vlan: name={{ item.name }} vlanid={{ item.vlanid }} state=unconfigured
+            eapi_username={{ eapi_username }}
+            eapi_password={{ eapi_password }}
+            eapi_hostname={{ eapi_hostname }}
+            eapi_port={{ eapi_port }}
+      with_items:
+         - {name: EXECUTIVE, vlanid: 401}
+         - {name: IT, vlanid: 402}
+         - {name: SALES, vlanid: 403}
+
